3117ad00cc1fb35f0ea62d645e2a0ed4df096a71
Prajwal2003 <prajwalgh13@gamil.com>
2024-12-23 14:33:23 +0530

    Adding test images

---
+++ Smart_Traffic_Managaement_system/logic.py
@@ -0,0 +1,4 @@
+import numpy as np
+
+array = np.zeros((4, 6))
+print(array)
--- Vehicle Counter/vehicle_counter_image.py
+++ Vehicle Counter/vehicle_counter_image.py
@@ -4,10 +4,8 @@ import numpy as np
 
 model = YOLO("../Test Yolo/Yolo_weights/yolov8l.pt")
 
-img = cv2.imread("download.jpeg")
-
+img = cv2.imread("images (1).jpeg")
 imgreg = img
-
 res = model(imgreg, stream=True)
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "pottedplant", "bed", "diningtable", "toilet", "tvmonitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
